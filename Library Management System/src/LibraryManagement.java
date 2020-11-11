ea72d41f977098715403fd6f6c7cbd7c43c8ef49
chriscarbajal <chriscarbajal@192.168.0.98>
2020-11-11 13:44:07 -0700

    -

--- Library Management System/src/LibraryManagement.java
+++ Library Management System/src/LibraryManagement.java
@@ -17,7 +17,7 @@ public class LibraryManagement {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if (!isConnectedToDb()) {
+		if (isConnectedToDb()) {
 			boolean running = true;
 			do {
 				int input = welcomeScreen();
